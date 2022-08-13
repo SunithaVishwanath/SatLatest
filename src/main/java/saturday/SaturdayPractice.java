b608d1e3a7bb17d7daeab3f350f525f22027dea2
sunithavishwanath <sunitha8682@gmail.com>
2022-08-13 17:02:13 +0530

    saturday

---
+++ src/main/java/saturday/SaturdayPractice.java
@@ -0,0 +1,13 @@
+package saturday;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class SaturdayPractice {
+
+	@Test
+	public void createCustomer() {
+		Reporter.log("Hi",true);
+	}
+
+}
---
+++ src/test/java/com/saturday/PracticeTest.java
@@ -0,0 +1,12 @@
+package com.saturday;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class PracticeTest {
+	@Test
+	public void testStart() {
+		Reporter.log("hello", true);
+	}
+
+}
